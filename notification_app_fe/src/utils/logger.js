51b8fa3e19273100944f16c99683a52679d10d2a
Tejaswini <tejaswinithota27@gmail.com>
2026-05-02 12:00:44 +0530

    cleaned project structure

---
+++ notification_app_fe/src/utils/logger.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJsYXNraG1pdGVqYXN3aW5pX3Rob3RhQHNybWFwLmVkdS5pbiIsImV4cCI6MTc3NzcwMTkxOCwiaWF0IjoxNzc3NzAxMDE4LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiZGQxMTFmZjItNzAyNy00MzYwLTg0NjEtYzRiZDZlZTI3MDNlIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoidGhvdGEgbGFrc2htaSB0ZWphc3dpbmkiLCJzdWIiOiI3NGRkYjJlMC0wZDgxLTRkZGUtODRmMy1lMzkzMmEwMWMxMDEifSwiZW1haWwiOiJsYXNraG1pdGVqYXN3aW5pX3Rob3RhQHNybWFwLmVkdS5pbiIsIm5hbWUiOiJ0aG90YSBsYWtzaG1pIHRlamFzd2luaSIsInJvbGxObyI6ImFwMjMxMTAwMTE1NzgiLCJhY2Nlc3NDb2RlIjoiUWticHhIIiwiY2xpZW50SUQiOiI3NGRkYjJlMC0wZDgxLTRkZGUtODRmMy1lMzkzMmEwMWMxMDEiLCJjbGllbnRTZWNyZXQiOiJSamRBU01mbm5aQWJlcVlNIn0.KJh0e2mdBEiuiaCukVkJn3Gseg5__mdpkEkUguAh0Ks";
+
+const Log = async (stack, level, pkg, message) => {
+  try {
+    await axios.post(
+      "/evaluation-service/logs",
+      {
+        stack,
+        level,
+        package: pkg,
+        message
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+  } catch (err) {
+    console.error("Logging failed:", err);
+  }
+};
+
+export default Log;
